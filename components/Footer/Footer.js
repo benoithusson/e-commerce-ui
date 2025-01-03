f49b572b5706aa6f70585e2408090806a4b3bedf
Benoît THIENNARD <benoitthiennard@MacBook-Pro-de-Benoit.local>
2025-01-03 21:43:27 +0100

    First commit

---
+++ components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.scss';
+
+export default function Footer(props) {
+    return (
+        <footer className={styles['footer-container']}>
+            FOOTER
+        </footer>
+    );
+}
---
+++ components/Header/Header.js
@@ -0,0 +1,9 @@
+import styles from './Header.module.scss';
+
+export default function Header(props) {
+    return (
+        <nav className={styles['header-container']}>
+            NAV
+        </nav>
+    );
+}
