793979cf5843c81478c8a5ae22a18c873161dd44
Benoît THIENNARD <benoitthiennard@mac.home>
2025-06-11 15:51:18 +0200

    Add try and catch into products list

--- components/_home/ProductsList/ProductsList.js
+++ components/_home/ProductsList/ProductsList.js
@@ -1,28 +1,37 @@
 import styles from "./ProductsList.module.scss";
 import ProductCard from "../ProductCard/ProductCard";
-import Button from "../../Button/Button";
 import Link from "next/link";
 
 export default async function ProductsList() {
-  const res = await fetch("http://localhost:3000/api/products");
-  const productsList = await res.json();
+  try {
+    const res = await fetch("http://localhost:3000/api/products");
 
-  return (
-    <div className={styles["products-list"]}>
-      <div className={styles["heading"]}>
-        <h3>New Arrivals</h3>
-        <Link href="">See all</Link>
+    if (!res.ok) {
+      throw new Error("La requête a échoué");
+    }
+
+    const productsList = await res.json();
+
+    return (
+      <div className={styles["products-list"]}>
+        <div className={styles["heading"]}>
+          <h3>New Arrivals</h3>
+          <Link href="">See all</Link>
+        </div>
+        <div className={styles["products"]}>
+          {productsList.map((product) => (
+            <ProductCard
+              key={product.id}
+              name={product.name}
+              price={product.price}
+              path={product.path}
+            />
+          ))}
+        </div>
       </div>
-      <div className={styles["products"]}>
-        {productsList.map((product) => (
-          <ProductCard
-            key={product.id}
-            name={product.name}
-            price={product.price}
-            path={product.path}
-          />
-        ))}
-      </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error("Erreur lors de la récupération:", error);
+    return null;
+  }
 }
